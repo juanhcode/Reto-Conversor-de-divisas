fdab733b60965e2c926c8d21c167c152ed78c9eb
Juan Hoyos <juanhoyos1347@gmail.com>
2022-03-27 00:20:51 -0500

    conversiones arregladas

--- app.js
+++ app.js
@@ -34,15 +34,11 @@ formulario.addEventListener('submit', (e) => {
         p.innerText = 'Formulario invalido digite una cantidad';
         div.appendChild(p);
         contenedor.appendChild(div);
-        setTimeout(() => {
-            contenedor.style.height = '230px';
-            div.style.display = 'none';
-        }, 3000);
     } else {
-        convertir(botonDinero.value, seleccionarDinero(),seleccionarMoneda());
+        let selectedOption = this.options[selectOpcion.selectedIndex];
+        convertir(selectedOption,botonDinero.value, seleccionarDinero(),seleccionarMoneda());
     }
-
-    //formulario.reset();
+    formulario.reset();
 })
 
 const agregarOpciones = () => {
@@ -57,54 +53,38 @@ const agregarOpciones = () => {
         selectOpcion.appendChild(option);
         selectOpcion2.appendChild(option2);
     })
-    agregarPrecios();
 }
 
-const agregarPrecios = () => {
-    //console.log(selectOpcion.getAttribute);
+const agregarPrecios = (elemento,array) => {
+    for (let i = 0; i < elemento.length; i++) {
+        let opt = elemento[i];
+        opt.value = array[i];
+    }
 }
+
 
 selectOpcion.addEventListener('change',
     function () {
         //let monedas = ['default,'dolar','Peso mexicano','Peso colombiano','Euro','libra esterlina'];
-        let preciosDolar = [0, 1, 20.07, 3.787, 0.91, 0.76];
-        //let monedas = ['default,'dolar','Peso mexicano','Peso colombiano','Euro','libra esterlina'];
+        let preciosDolar = [0, 1, 20.07, 3798.54, 0.91, 0.76];
         let pesosMexicano = [0, 0.050, 1, 189.60, 0.045, 0.038];
-        //let monedas = ['default,'dolar','Peso mexicano','Peso colombiano','Euro','libra esterlina'];
         let pesosColombiano = [0,0.00026,0.0053,1,0.00024,0.00020];
-        //let monedas = ['default,'dolar','Peso mexicano','Peso colombiano','Euro','libra esterlina'];
-        let euros = [0,1.10,22.00,4.172,1,0.83];
-        //let monedas = ['default,'dolar','Peso mexicano','Peso colombiano','Euro','libra esterlina'];
-        let libras = [0,1.32,26.43,5.010,1.20,1];
+        let euros = [0,1.10,22.00,4172.51,1,0.83];
+        let libras = [0,1.32,26.43,5010.46,1.20,1];
+
 
         let selectedOption = this.options[selectOpcion.selectedIndex];
         if (selectedOption.text === 'Dolar') {
-            for (let i = 0; i < selectOpcion2.length; i++) {
-                let opt = selectOpcion2[i];
-                opt.value = preciosDolar[i];
-            }
+            agregarPrecios(selectOpcion2,preciosDolar);
         } else if (selectedOption.text === 'Peso Mexicano') {
-            for (let i = 0; i < selectOpcion2.length; i++) {
-                let opt = selectOpcion2[i];
-                opt.value = pesosMexicano[i];
-            }
+            agregarPrecios(selectOpcion2,pesosMexicano);
         } else if (selectedOption.text === 'Peso Colombiano') {
-            for (let i = 0; i < selectOpcion2.length; i++) {
-                let opt = selectOpcion2[i];
-                opt.value = pesosColombiano[i];
-            }
+            agregarPrecios(selectOpcion2,pesosColombiano);
         } else if (selectedOption.text === 'Euro') {
-            for (let i = 0; i < selectOpcion2.length; i++) {
-                let opt = selectOpcion2[i];
-                opt.value = euros[i];
-            }
+            agregarPrecios(selectOpcion2,euros);
         } else if (selectedOption.text === 'Libra Esterlina') {
-            for (let i = 0; i < selectOpcion2.length; i++) {
-                let opt = selectOpcion2[i];
-                opt.value = libras[i];
-            }
+            agregarPrecios(selectOpcion2,libras);
         }
-
     }
 );
 
@@ -118,21 +98,16 @@ const seleccionarMoneda = () => {
     return selectedOption2.text;
 }
 
-const convertir = (cantidad, valorActual,moneda) => {
-    let resultado = cantidad * valorActual;
+const convertir = (monedaActual,cantidad, valorActual,moneda) => {
+    let resultado = new Intl.NumberFormat('es-Mx').format(cantidad * valorActual);
     const contenedor = document.querySelector('.contenedor');
-    //const div = document.createElement('div');
-    //div.remove();
     contenedor.style.height = '280px';
-    //div.classList.add('new-div');
-    //div.style.display = 'block';
     const p = document.createElement('p');
     p.classList.add('parrafo');
-    p.innerText = ` ${resultado.toFixed(4)} ${moneda}`;
-    p.style.fontSize = '33px'
+    p.innerText = `$${cantidad} ${monedaActual.text} ->  $${resultado} ${moneda}`;
+    p.style.fontSize = '25px'
     p.style.color = 'black';
     p.style.textAlign='center';
     p.style.fontFamily = 'Helvetica';
-    //div.appendChild(p);
     contenedor.appendChild(p);
 }
